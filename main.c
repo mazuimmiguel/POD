2e69733759b445d8bc80838a874217aeeae7a385
Miguel M Silva <mazuimmiguel@gmail.com>
2019-07-01 10:28:55 -0300

    Finalizado a compressao
    Ta uma gambiarra, mas funciona.
    Ainda falta a decompressao.

--- main.c
+++ main.c
@@ -5,19 +5,34 @@
 void comprimir(char* arquivo){
 	int i=0; 
 	int cont = 0;
-	char[200] temp;
+	char temp[200];
+	char salvar[5];
 	char comparar;
 	
 	FILE* arq = fopen(arquivo,"r");
-	fgets(temp,200,arq);
 	FILE* comprimido = fopen("comprimido.txt","w");
+	fgets(temp,200,arq);//pega o texto
 	
 	do{
-		comparar = temp[i];
-		do{
+		if(temp[i] == '\0'){//verifica se acabou o arquivos
 			break;
+		}
+		
+		comparar = temp[i];//Pega o caractere para comparar
+		cont=1;//o primeiro
+		
+		do{
+			if(temp[i+1] == comparar && temp[i+1] != '\0'){//se o proximo for igual e não for o final
+				cont++;//aumenta a quantidade
+			}else{
+				break;
+			}
+			i++;
 		}while(1);
-		break;
+		
+		sprintf(salvar,"%c%i",comparar,cont);//comprime e salva
+		fwrite(salvar, sizeof(char)*strlen(salvar), 1, comprimido);
+		i++;
 	}while(1);
 
 	
@@ -29,6 +44,7 @@ int main( int argc, char *argv[ ]){
 		exit(1);
 	}
 	
+	printf("Comprimindo Arquivo: %s\n",argv[1]);
 	comprimir(argv[1]);
 	
 }
